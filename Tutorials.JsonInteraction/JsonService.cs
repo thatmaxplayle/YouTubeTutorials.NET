02bcf6b7a4e5abdf8a1d8bdc584781fa1c7a75cf
Max Playle <maxplayle04@gmail.com>
2020-10-03 16:17:55 +0100

    Add project files.

---
+++ Tutorials.JsonInteraction/JsonService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Tutorials.JsonInteraction
+{
+    public class JsonService
+    {
+
+        public static void ReadJsonFile(string jsonFileIn)
+        {
+
+            dynamic jsonFile = JsonConvert.DeserializeObject(File.ReadAllText(jsonFileIn));
+
+            Console.WriteLine($"Today's Video is: {jsonFile["today_video"]}");
+
+            Console.WriteLine($"Today's video is about: {jsonFile["video_information"]["title"]}");
+
+            JToken VideoTitle = jsonFile.SelectToken("video_information.creator");
+            JToken VideoPurpose = jsonFile.SelectToken("video_information.purpose");
+
+            Console.WriteLine($"Creator's Name: {VideoTitle}");
+            Console.WriteLine($"Purpose of this video: {VideoPurpose}");
+
+
+        }
+
+
+    }
+}
---
+++ Tutorials.JsonInteraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Tutorials.JsonInteraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to my application!");
+            JsonService.ReadJsonFile(AskForJsonFileName());
+        }
+
+        public static string AskForJsonFileName()
+        {
+            BEGIN:
+            Console.Write("\nWhat is the name of your JSON file? ");
+            var response = Console.ReadLine();
+            if (File.Exists(response))
+            {
+                return response;
+            }
+            else
+            {
+                Console.Write("\nError: File doesn't exist!");
+                goto BEGIN;
+            }
+        }
+
+    }
+}
